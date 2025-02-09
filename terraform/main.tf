da82cd8bd307c772698894ceb0947265a1dc5203
pabhi18 <pabhi4881@gmail.com>
2025-02-09 05:47:37 +0530

    Update Terraform configurations and set up Ingress for EKS deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,57 @@ module "vpc" {
   private_subnets = var.private_subnets_cidr
   public_subnets  = var.public_subnets_cidr
   enable_nat_gateway = true
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+}
+
+# ECR policy
+resource "aws_iam_policy" "ecr_policy" {
+  name = "eks-ecr-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# ALB policy
+resource "aws_iam_policy" "alb_policy" {
+  name = "eks-alb-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:*",
+          "ec2:CreateSecurityGroup",
+          "ec2:Describe*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 module "eks" {
@@ -26,6 +77,11 @@ module "eks" {
       desired_size = 1
       instance_types = ["t3.small"]
       capacity_type  = "SPOT"
+
+      iam_role_additional_policies = {
+        AmazonECR_Policy = aws_iam_policy.ecr_policy.arn
+        ALBIngress_Policy = aws_iam_policy.alb_policy.arn
+      }
     }
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         	   = "node-app-tfstate"
+    key              	 = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table     = "node_app_lockid"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
-
